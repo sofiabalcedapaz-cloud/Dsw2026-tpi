c180372dc73458b89ca52729b126647e4f7d5734
Victoria Barrientos <Victoria.Barrientos@alu.frt.utn.edu.ar>
2026-07-31 21:42:29 -0300

    refactor: actualiza validacion de DNI y login de paciente

---
+++ Dsw2026Tpi.Data/Migrations/Domain/20260731185459_UpdatePatientEntity.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dsw2026Tpi.Data.Migrations.Domain
+{
+    /// <inheritdoc />
+    public partial class UpdatePatientEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Patients");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "Patients",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Patients",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Patients_Dni",
+                table: "Patients",
+                column: "Dni",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Patients_Dni",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Patients");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Patients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
